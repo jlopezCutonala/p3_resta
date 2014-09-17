f5aa587258a59385ca1a7fbc4085c538555eb898
Jonathan Lopez <jlopez.cutonala@gmail.com>
2014-09-16 21:50:41 -0500

    Mostrar el resultado

--- p3_resta/Program.cs
+++ p3_resta/Program.cs
@@ -18,6 +18,10 @@ namespace p3_resta
 			int numero1=int.Parse(Console.ReadLine());
 			Console.WriteLine("Dame el segundo número:");
 			int numero2=int.Parse(Console.ReadLine());
+			
+			int resultado = numero1 - numero2;
+			Console.WriteLine("El resultado es " + resultado);
+			Console.ReadLine();
 		}
 	}
 }
